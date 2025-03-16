43a519b3752370f818df000e2ec140fb03149d91
Luís Copetti <lhcopetti@gmail.com>
2025-03-16 17:27:46 -0300

    Add logging in satori reader provider

--- src/main/kotlin/com/copetti/srcli/provider/satori/webcrawler/WebCrawlerSatoriReaderProvider.kt
+++ src/main/kotlin/com/copetti/srcli/provider/satori/webcrawler/WebCrawlerSatoriReaderProvider.kt
@@ -1,11 +1,12 @@
 package com.copetti.srcli.provider.satori.webcrawler
 
 import com.copetti.srcli.domain.gateway.SatoriReaderProvider
+import com.copetti.srcli.domain.model.*
 import com.copetti.srcli.provider.satori.SatoriReaderWebConstants.SATORI_READER_API_URL
 import com.copetti.srcli.provider.satori.SatoriReaderWebConstants.SATORI_READER_URL
 import com.copetti.srcli.provider.satori.SatoriReaderWebConstants.SERIES_URL
 import com.copetti.srcli.provider.satori.SatoriReaderWebConstants.SIGNIN_URL
-import com.copetti.srcli.domain.model.*
+import mu.KotlinLogging
 import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -15,7 +16,11 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 class WebCrawlerSatoriReaderProvider : SatoriReaderProvider {
+
+    private val logger = KotlinLogging.logger { }
+
     override fun login(credentials: SatoriReaderCredentials): SatoriReaderLoginToken {
+        logger.info { "SATORI_READER_LOGIN_REQUEST" }
         val formBody = FormBody.Builder()
             .add("username", credentials.username)
             .add("password", credentials.password)
@@ -31,6 +36,7 @@ class WebCrawlerSatoriReaderProvider : SatoriReaderProvider {
             .build()
 
         val loginResponse = client.newCall(signInRequest).execute()
+        logger.info { "SATORI_READER_LOGIN_RESPOSE | status: ${loginResponse.code}" }
         val token = getSessionTokenFromResponse(loginResponse)
         return SatoriReaderLoginToken(sessionToken = token)
     }
@@ -48,11 +54,13 @@ class WebCrawlerSatoriReaderProvider : SatoriReaderProvider {
     }
 
     override fun fetchSeries(): List<SatoriReaderSeriesReference> {
+        logger.info { "SATORI_READER_FETCH_SERIES_REQUEST" }
         val client = OkHttpClient()
         val request = Request.Builder()
             .url(SATORI_READER_URL + SERIES_URL)
             .build()
         val response = client.newCall(request).execute()
+        logger.info { "SATORI_READER_FETCH_SERIES_RESPONSE | status: ${response.code}" }
 
         val document = Jsoup.parse(response.body?.string() ?: "")
 
@@ -65,9 +73,11 @@ class WebCrawlerSatoriReaderProvider : SatoriReaderProvider {
             .filterNot(String::isEmpty)
             .map { link -> SatoriReaderSeriesReference(link = SATORI_READER_URL + link) }
             .toList()
+            .also { logger.info { "SATORI_READER_FETCH_SERIES | seriesCount: ${it.size}" } }
     }
 
     override fun fetchSeriesContent(request: FetchSeriesContentRequest): SatoriReaderSeriesContent {
+        logger.info { "SATORI_READER_FETCH_SERIES_CONTENT_REQUEST | series: ${request.series}" }
 
         val response = OkHttpClient().newCall(
             Request.Builder()
@@ -75,6 +85,8 @@ class WebCrawlerSatoriReaderProvider : SatoriReaderProvider {
                 .header("Cookie", "SessionToken=${request.token.sessionToken}")
                 .build()
         ).execute()
+        logger.info { "SATORI_READER_FETCH_SERIES_CONTENT_RESPONSE | status: ${response.code}" }
+
         val body: String = response.body?.string() ?: ""
         val document = Jsoup.parse(body)
         val seriesTitle = document.getElementsByClass("series-detail-title-description-area")
@@ -88,6 +100,8 @@ class WebCrawlerSatoriReaderProvider : SatoriReaderProvider {
     }
 
     override fun resetReadingProgress(request: ResetEditionReadingProgressRequest) {
+        logger.info { "SATORI_READER_RESET_PROGRESS_REQUEST | editionUrl: ${request.edition.urlPath}" }
+
         OkHttpClient().newCall(
             Request.Builder()
                 .url(SATORI_READER_API_URL + request.edition.urlPath + "/status")
@@ -95,6 +109,7 @@ class WebCrawlerSatoriReaderProvider : SatoriReaderProvider {
                 .put("""{"type":"read-state","value":"0"}""".toRequestBody())
                 .build()
         ).execute()
+            .also { logger.info { "SATORI_READER_RESET_PROGRESS_RESPONSE | status: ${it.code}" } }
     }
 
     private fun mapEpisodes(episode: Element): SatoriReaderEpisode {
